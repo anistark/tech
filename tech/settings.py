9f015b7a161844baeb6e4ef71f01d12ebebc0a22
Kumar Anirudha <anirudhastark@yahoo.com>
2015-05-14 23:50:15 +0530

    regionaire api set initiated

--- tech/settings.py
+++ tech/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'techsite'
+    'techsite',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -94,3 +95,16 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
     'tech/templates/',
 )
+
+# REST_FRAMEWORK = {
+#     # Use Django's standard `django.contrib.auth` permissions,
+#     # or allow read-only access for unauthenticated users.
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     ]
+# }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
--- techsite/views.py
+++ techsite/views.py
@@ -4,3 +4,4 @@ from django.shortcuts import render
 def home(request):
 	context = {'title':'AniTech'}
 	return render(request,'index.html',context)
+
